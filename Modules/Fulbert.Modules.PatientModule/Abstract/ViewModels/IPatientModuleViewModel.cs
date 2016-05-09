1f7097cb664c15bf58c621465e7474798b780ecb
Michał Józef Gąsior <michaljgasior@outlook.com>
2016-05-10 00:19:35 +0200

    Trying to set up the localization

--- Modules/Fulbert.Modules.PatientModule/Abstract/ViewModels/IPatientModuleViewModel.cs
+++ Modules/Fulbert.Modules.PatientModule/Abstract/ViewModels/IPatientModuleViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using Fulbert.Infrastructure.Abstract.Mvvm;
+using Prism.Commands;
 
 namespace Fulbert.Modules.PatientModule.Abstract.ViewModels
 {
     public interface IPatientModuleViewModel : IViewModel
     {
-
+        DelegateCommand SaveUserCommand { get; }
     }
 }
--- Modules/Fulbert.Modules.PatientModule/ViewModels/PatientModuleViewModel.cs
+++ Modules/Fulbert.Modules.PatientModule/ViewModels/PatientModuleViewModel.cs
@@ -1,8 +1,21 @@
-﻿using Fulbert.Modules.PatientModule.Abstract.ViewModels;
+﻿using System;
+using Fulbert.Modules.PatientModule.Abstract.ViewModels;
+using Prism.Commands;
 
 namespace Fulbert.Modules.PatientModule.ViewModels
 {
     public class PatientModuleViewModel : IPatientModuleViewModel
     {
+        public DelegateCommand SaveUserCommand { get; private set; }
+
+        public PatientModuleViewModel()
+        {
+            SaveUserCommand = new DelegateCommand(OnSaveUserCommand);
+        }
+
+        private void OnSaveUserCommand()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
